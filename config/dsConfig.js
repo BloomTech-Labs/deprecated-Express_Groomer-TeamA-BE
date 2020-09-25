758bf57cd6e0dd10119dd426b2bef12009095a9c
Jake Mallory <4095+tinomen@users.noreply.github.com>
2020-09-24 18:04:24 -0600

    remove ds api service

--- config/dsConfig.js
+++
@@ -1,9 +0,0 @@
-var dotenv = require('dotenv');
-dotenv.config({ path: '../.env' });
-
-module.exports = {
-  baseURL: process.env.DS_API_URL,
-  headers: {
-    Authorization: process.env.DS_API_TOKEN,
-  },
-};
