dbf631950b7244c4ba23619ee1a1a60b87ca3a68
tanveersaleem786 <tanveer.saleem786@gmail.com>
2020-10-20 20:12:06 -0400

    Add Fields in profile migration and seeds

--- data/seeds/001_profiles.js
+++ data/seeds/001_profiles.js
@@ -2,6 +2,10 @@ const faker = require('faker');
 
 const profiles = [...new Array(5)].map((i, idx) => ({
   id: idx === 0 ? '00ulthapbErVUwVJy4x6' : faker.random.alphaNumeric(20),
+  profile_id: faker.random.number(100) + faker.random.number(50),
+  password: faker.random.number(5),
+  user_type: 'Customer',
+  gender: 'Male',
   avatarUrl: faker.image.avatar(),
   email: idx === 0 ? 'llama001@maildrop.cc"' : faker.internet.email(),
   name:
@@ -10,12 +14,18 @@ const profiles = [...new Array(5)].map((i, idx) => ({
       : `${faker.name.firstName()} ${faker.name.lastName()}`,
 }));
 
-exports.seed = function (knex) {
-  // Deletes ALL existing entries
-  return knex('profiles')
-    .del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('profiles').insert(profiles);
-    });
+// exports.seed = function (knex) {
+//   // Deletes ALL existing entries
+//   return knex('profiles')
+//     .del()
+//     .then(function () {
+//       // Inserts seed entries
+//       return knex('profiles').insert(profiles);
+//     });
+// };
+
+
+exports.seed = async (knex) => {
+  // Insert profiles
+  await knex("profiles").insert(profiles)
 };
