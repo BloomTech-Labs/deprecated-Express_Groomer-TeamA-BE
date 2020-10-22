f377540134d25f8dff624f794a4b9b3e6d608003
tanveersaleem786 <tanveer.saleem786@gmail.com>
2020-10-21 22:29:09 -0400

    Create migration for user_ratings table

---
+++ data/migrations/20201021211952_create-user-ratings.js
@@ -0,0 +1,30 @@
+exports.up = async (knex) => {
+    await knex.schema
+      .createTable('user_ratings', function (table) {
+        table.increments("id")
+        table.string('profile_id')
+        // forces integer to be positive
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('profiles')
+        .onDelete("CASCADE")
+		.onUpdate('CASCADE')
+        table.integer('service_id')
+        // forces integer to be positive
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('services')
+        .onDelete("CASCADE")
+		.onUpdate('CASCADE')
+        table.string('review_point').notNullable()
+        table.string('feedback').default(null)
+        table.timestamps(true, true)
+      });
+  };
+  
+  exports.down = async (knex) => {
+    await knex.schema.dropTableIfExists('user_ratings')
+    await knex.schema.dropTableIfExists('profiles')
+  };
