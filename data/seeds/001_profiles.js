ff290f99f251607f43b46163c6fc6af3b52e4281
tanveersaleem786 <tanveer.saleem786@gmail.com>
2020-10-21 22:01:06 -0400

    Remove Extra Fields profile_id, password, gender

--- data/seeds/001_profiles.js
+++ data/seeds/001_profiles.js
@@ -1,11 +1,8 @@
 const faker = require('faker');
 
 const profiles = [...new Array(5)].map((i, idx) => ({
-  id: idx === 0 ? '00ulthapbErVUwVJy4x6' : faker.random.alphaNumeric(20),
-  profile_id: faker.random.number(100) + faker.random.number(50),
-  password: faker.random.number(5),
-  user_type: 'Customer',
-  gender: 'Male',
+  id: idx === 0 ? '00ulthapbErVUwVJy4x6' : faker.random.alphaNumeric(20),  
+  user_type: 'Customer',  
   avatarUrl: faker.image.avatar(),
   email: idx === 0 ? 'llama001@maildrop.cc"' : faker.internet.email(),
   name:
