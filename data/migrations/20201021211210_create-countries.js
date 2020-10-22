3f95a5932ced0d1a0337726406b0d02934ec9133
tanveersaleem786 <tanveer.saleem786@gmail.com>
2020-10-21 22:16:49 -0400

    Create migration for countries table

---
+++ data/migrations/20201021211210_create-countries.js
@@ -0,0 +1,13 @@
+exports.up = async (knex) => {
+    await knex.schema
+      .createTable('countries', function (table) {
+        table.increments("id")
+        table.string('sort_name', 3)
+        table.string('name')
+        table.string('phone_code')
+      });
+  };
+  
+  exports.down = async (knex) => {
+    await knex.schema.dropTableIfExists('countries')
+  };
