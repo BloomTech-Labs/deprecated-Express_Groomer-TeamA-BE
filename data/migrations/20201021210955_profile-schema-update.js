174ed196a6d7ad830a504ebc3342b266dcca2fd2
tanveersaleem786 <tanveer.saleem786@gmail.com>
2020-10-21 22:06:06 -0400

    Add user_type and rating field in profile migration

---
+++ data/migrations/20201021210955_profile-schema-update.js
@@ -0,0 +1,15 @@
+exports.up = async (knex) => {
+    await knex.schema
+        .table('profiles', function(table) {
+            table.enu('user_type', ['Customer', 'Groomer']).notNullable()
+            table.float('rating')
+        })
+};
+
+exports.down = async (knex) => {
+    await knex.schema
+        .table('profiles', function(table) {
+            table.dropColumn('user_type')
+            table.dropColumn('rating')
+        });
+};
