c17fbe51bec15268e736ef639a0ba189f9eb43ea
tanveersaleem786 <tanveer.saleem786@gmail.com>
2020-10-25 19:33:25 -0400

    Fix the space issue

--- data/migrations/20201021210955_profile-schema-update.js
+++ data/migrations/20201021210955_profile-schema-update.js
@@ -1,15 +1,13 @@
 exports.up = async (knex) => {
-    await knex.schema
-        .table('profiles', function(table) {
+    await knex.schema.table('profiles', function(table) {
             table.enu('user_type', ['Customer', 'Groomer']).notNullable()
             table.float('rating')
-        })
+    })
 };
 
 exports.down = async (knex) => {
-    await knex.schema
-        .table('profiles', function(table) {
+    await knex.schema.table('profiles', function(table) {
             table.dropColumn('user_type')
             table.dropColumn('rating')
-        });
+    });
 };
