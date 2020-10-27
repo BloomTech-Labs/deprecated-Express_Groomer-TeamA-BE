adcae321fbbe21aed4fb1667cd3cb3701391a00d
tanveersaleem786 <tanveer.saleem786@gmail.com>
2020-10-26 22:24:12 -0400

    Fix the typo error short_name column

--- data/migrations/20201021211210_create-countries.js
+++ data/migrations/20201021211210_create-countries.js
@@ -1,7 +1,7 @@
 exports.up = async (knex) => {
   await knex.schema.createTable('countries', function (table) {
     table.increments('id');
-    table.string('sort_name', 3);
+    table.string('short_name', 3);
     table.string('name');
     table.string('phone_code');
   });
