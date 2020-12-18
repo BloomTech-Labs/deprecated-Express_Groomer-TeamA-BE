e57476dcb50fded49e8681847caf42cce1d3885b
Cody Brewer <codybrewerdev@gmail.com>
2020-12-17 18:48:15 -0700

    feat(appointmentsRouter.js-appointmentsModel.js): GET url/appointments route and getAll db model function

--- api/appointments/appointmentsRouter.js
+++ api/appointments/appointmentsRouter.js
@@ -4,8 +4,10 @@ const AppointmentsModel = require('./appointmentsModel');
 const router = express.Router();
 
 router.post('/', authRequired, async (req, res) => {
+  const customer_id = req.profile.id;
   try {
     const appointment = req.body;
+    appointment.customer_id = customer_id;
 
     const newAppointment = await AppointmentsModel.create(appointment);
 
@@ -13,6 +15,18 @@ router.post('/', authRequired, async (req, res) => {
   } catch (e) {
     console.error(e.stack);
     res.status(500).json({ error: 'Error creating new appointment' });
+  }
+});
+
+router.get('/', authRequired, async (req, res) => {
+  const profileId = req.profile.id;
+
+  try {
+    const appointments = await AppointmentsModel.getAll(profileId);
+    res.status(200).json(appointments);
+  } catch (e) {
+    console.error(e.stack);
+    res.status(500).json({ error: 'Error getting appointments' });
   }
 });
 
