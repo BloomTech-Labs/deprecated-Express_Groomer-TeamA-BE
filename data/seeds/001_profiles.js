1e4f52595e7ca9cee34fbe87e735235447bdaf19
Cody Brewer <codybrewerdev@gmail.com>
2021-01-09 09:16:08 -0700

    refactor(001_profiles.js): hard code in an id for groomer profile

--- data/seeds/001_profiles.js
+++ data/seeds/001_profiles.js
@@ -1,7 +1,12 @@
 const faker = require('faker');
 
 const profiles = [...new Array(5)].map((i, idx) => ({
-  id: idx === 0 ? '00ulthapbErVUwVJy4x6' : faker.random.alphaNumeric(20),
+  id:
+    idx === 0
+      ? '00ulthapbErVUwVJy4x6'
+      : idx === 2
+      ? 'yonp6i2k2xll5qq8mvgq'
+      : faker.random.alphaNumeric(20),
   user_type: parseInt(idx) <= 1 ? 'Customer' : 'Groomer',
   avatarUrl: faker.image.avatar(),
   email: idx === 0 ? 'llama001@maildrop.cc"' : faker.internet.email(),
