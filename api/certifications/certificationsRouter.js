ead0df5384e34d83eb1c67627e5dc67f6a79f7a3
Cody Brewer <codybrewerdev@gmail.com>
2021-01-09 16:54:33 -0700

    GET request to  certification/Groomer/{groomerId} swagger docs

--- api/certifications/certificationsRouter.js
+++ api/certifications/certificationsRouter.js
@@ -22,7 +22,7 @@ const router = require('express').Router;
  *          type: string
  *        insitute:
  *          type: string
- *        image
+ *        image:
  *          type: string
  *        date_issued:
  *          type: integer
@@ -39,6 +39,55 @@ const router = require('express').Router;
  *          'https://headtotailpetgrooming.weebly.com/uploads/6/0/2/9/60294035/attendence_1_orig.jpg'
  *        date_issued: 1610224700
  *        date_expired: 1925757500
+ *  parameters:
+ *    groomerId:
+ *      name: id
+ *      in: path
+ *      description: ID of the groomer to get certifications for
+ *      required: true
+ *      example: '00ultwew80Onb2vOT4x6'
+ *      schema:
+ *        type: string
+ * /certifications/Groomer/{groomerId}:
+ *  get:
+ *    description: Find all certifcations by groomerID
+ *    summary: Returns all certifications for a groomer
+ *    secuirty:
+ *      - okata: []
+ *    tags:
+ *      - certification
+ *    parameters:
+ *      - $ref: '#/components/parameters/groomerId'
+ *    responses:
+ *      200:
+ *        description: groomer certification data
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: array
+ *              items:
+ *                $ref: '#/components/schemas/Certification'
+ *              example:
+ *                - id: 0
+ *                  groomer_id: '00ultwew80Onb2vOT4x6'
+ *                  title: 'Long-Legged Terriers'
+ *                  institute: 'Devry Institute of Dog Grooming'
+ *                  image:
+ *                    'https://headtotailpetgrooming.weebly.com/uploads/6/0/2/9/60294035/attendence_1_orig.jpg'
+ *                  date_issued: 1610224700
+ *                  date_expired: 1925757500
+ *                - id: 1
+ *                  groomer_id: '00ultwew80Onb2vOT4x6'
+ *                  title: 'Short-Legged Terriers'
+ *                  institute: 'Devry Institute of Dog Grooming'
+ *                  image:
+ *                    'https://headtotailpetgrooming.weebly.com/uploads/6/0/2/9/60294035/attendence_1_orig.jpg'
+ *                  date_issued: 1610224700
+ *                  date_expired: 1925757500
+ *      401:
+ *        $ref: '#/compoinents/responses/UnauthorizedError'
+ *      404:
+ *        description: 'no certifications found'
  */
 
 module.exports = router;
