7ebd959839ab03c890291f45d57a2869ebc6f01a
Cody Brewer <codybrewerdev@gmail.com>
2021-01-09 13:43:50 -0700

    fix(001_profiles.js): set an okta id to a groomer id when seeding profiles

--- data/seeds/001_profiles.js
+++ data/seeds/001_profiles.js
@@ -1,7 +1,12 @@
 const faker = require('faker');
 
 const profiles = [...new Array(5)].map((i, idx) => ({
-  id: idx === 0 ? '00ulthapbErVUwVJy4x6' : faker.random.alphaNumeric(20),
+  id:
+    idx === 0
+      ? '00ulthapbErVUwVJy4x6'
+      : idx === 2
+      ? '00ultwew80Onb2vOT4x6'
+      : faker.random.alphaNumeric(20),
   user_type: parseInt(idx) <= 1 ? 'Customer' : 'Groomer',
   avatarUrl: faker.image.avatar(),
   email: idx === 0 ? 'llama001@maildrop.cc"' : faker.internet.email(),
