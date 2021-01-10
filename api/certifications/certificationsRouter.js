9e3f5c32e49217c283c323abd193840b5f2218c3
ClydeFrog04 <egan.randal@gmail.com>
2021-01-09 17:28:15 -0700

    feat(certificationsrouter.js): router endpoint for creating a new cert

--- api/certifications/certificationsRouter.js
+++ api/certifications/certificationsRouter.js
@@ -1,3 +1,17 @@
 const router = require('express').Router;
+const authRequired = require('../middleware/authRequired');
+const certificationsModel = require('./certificationsModel');
+
+router.post('/', authRequired, async (req, res) => {
+  try {
+    const cert = { ...req.body, groomer_id: req.profile.id };
+    const newCert = certificationsModel.createCertificate(cert);
+
+    res.status(201).json({ newCert });
+  } catch (e) {
+    console.log(e.stack);
+    res.status(500).json({ error: 'Error creating certificate.' });
+  }
+});
 
 module.exports = router;
