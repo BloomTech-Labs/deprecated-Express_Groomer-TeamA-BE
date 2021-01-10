4363069122e1354f0fb7180d706e45308d5f01a7
ClydeFrog04 <egan.randal@gmail.com>
2021-01-09 17:32:00 -0700

    feat(certificationsrouter.js): added middleware to the post route

--- api/certifications/certificationsRouter.js
+++ api/certifications/certificationsRouter.js
@@ -1,8 +1,9 @@
 const router = require('express').Router;
 const authRequired = require('../middleware/authRequired');
+const { verifyProfileIsGroomer } = require('../middleware/certifications');
 const certificationsModel = require('./certificationsModel');
 
-router.post('/', authRequired, async (req, res) => {
+router.post('/', authRequired, verifyProfileIsGroomer, async (req, res) => {
   try {
     const cert = { ...req.body, groomer_id: req.profile.id };
     const newCert = certificationsModel.createCertificate(cert);
