4f58cb94bbbf3e5b93cbd35482f70a7b406548a2
Cody Brewer <codybrewerdev@gmail.com>
2021-01-11 12:09:19 -0700

    getById certification db model function

--- api/certifications/certificationsModel.js
+++ api/certifications/certificationsModel.js
@@ -1,7 +1,8 @@
 const db = require('../../data/db-config');
 
-
 const getBy = (filter) => db('certifications').where(filter);
+
+const getById = (id) => db('certifications').where(id).first().select('*');
 
 const createCertificate = (certificate) => {
   return db('certifications').insert(certificate).returning('*');
@@ -14,6 +15,7 @@ const update = () => {};
 module.exports = {
   createCertificate,
   getBy,
+  getById,
   remove,
   update,
 };
