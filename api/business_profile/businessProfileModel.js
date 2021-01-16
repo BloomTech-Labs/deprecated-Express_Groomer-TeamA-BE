38578434120b79bc551a9ed71e5b077ba07c0909
Cody Brewer <codybrewerdev@gmail.com>
2021-01-16 13:06:33 -0700

    fix(businessProfileModel.js): return business profile information back as expected schema

--- api/business_profile/businessProfileModel.js
+++ api/business_profile/businessProfileModel.js
@@ -1,22 +1,26 @@
 const db = require('../../data/db-config');
 
 const createBusinessProfile = (profile, groomerID) => {
-  profile.groomer_id = groomerID;
+  profile.profile_id = groomerID;
   return db('business_profiles').insert(profile).returning('*');
 };
 
-const createCoverImage = (image, groomerID) => {
-  image.groomer_id = groomerID;
+const createCoverImage = (image) => {
+  console.log({ image });
   return db('groomer_cover_images').insert(image).returning('*');
 };
 
 const getBusinessProfile = async (groomerID) => {
   // use promise.all
   // profile = get everything from business_profile table where groomer_id = groomerID
+  const location = await db('locations')
+    .where({ profile_id: groomerID })
+    .first()
+    .select('*');
   const promises = [];
   const business_profile = db('business_profiles')
     .where({
-      groomer_id: groomerID,
+      profile_id: groomerID,
     })
     .first()
     .select('*');
@@ -27,20 +31,21 @@ const getBusinessProfile = async (groomerID) => {
     })
     .returning('*');
   // location = get all groomer locations where profile_id = groomerID
-  const location = db('locations')
-    .where({ profile_id: groomerID })
-    .first()
-    .select('*');
   // services = get all locationsServices where location_id = locations[id]?
   const services = db('location_services')
     .where({ location_id: location.id })
     .returning('*');
 
-  promises.push(business_profile, groomer_cover_images, location, services);
-
+  promises.push(business_profile, groomer_cover_images, services);
   const groomer_profile_info = await Promise.all(promises);
-
-  return groomer_profile_info;
+  console.info({ gp_info: groomer_profile_info });
+  const business_profile_info = {
+    business_profile: groomer_profile_info[0],
+    groomer_cover_images: groomer_profile_info[1],
+    services: groomer_profile_info[2],
+    location,
+  };
+  return business_profile_info;
 };
 
 module.exports = {
