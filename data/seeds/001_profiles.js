2ae83484d8b8124da6fa844c23e487874b263c60
Cody Brewer <codybrewerdev@gmail.com>
2021-01-30 14:59:49 -0700

    fix(001_profiles.js): seed all okta accounts as users. odd user number is customer even is groomer

--- data/seeds/001_profiles.js
+++ data/seeds/001_profiles.js
@@ -1,28 +1,71 @@
 const faker = require('faker');
 
-const profiles = [...new Array(5)].map((i, idx) => ({
-  id:
-    idx === 0
-      ? '00ulthapbErVUwVJy4x6'
-      : idx === 2
-      ? '00ultwew80Onb2vOT4x6'
-      : faker.random.alphaNumeric(20),
-  user_type: parseInt(idx) <= 1 ? 'Customer' : 'Groomer',
-  avatarUrl: faker.image.avatar(),
-  email:
-    idx === 0
-      ? 'llama001@maildrop.cc'
-      : idx === 2
-      ? 'llama002@maildrop.cc'
-      : faker.internet.email(),
-  name:
-    idx === 0
-      ? 'Test001 User'
-      : idx === 2
-      ? 'Test002 User'
-      : `${faker.name.firstName()} ${faker.name.lastName()}`,
-  certifications: idx > 1 ? ['Sporting', 'Long-Legged Terriers'] : null,
-}));
+const profiles = [
+  {
+    id: '00ulthapbErVUwVJy4x6',
+    user_type: 'Customer',
+    avatarUrl: faker.image.avatar(),
+    email: 'llama001@maildrop.cc',
+    name: `${faker.name.firstName()} ${faker.name.lastName()}`,
+    certifications: null,
+  },
+  {
+    id: '00ultwew80Onb2vOT4x6',
+    user_type: 'Groomer',
+    avatarUrl: faker.image.avatar(),
+    email: 'llama002@maildrop.cc',
+    name: `${faker.name.firstName()} ${faker.name.lastName()}`,
+    certifications: ['Dogs', 'Cats'],
+  },
+  {
+    id: '00ultx74kMUmEW8054x6',
+    user_type: 'Customer',
+    avatarUrl: faker.image.avatar(),
+    email: 'llama003@maildrop.cc',
+    name: `${faker.name.firstName()} ${faker.name.lastName()}`,
+    certifications: null,
+  },
+  {
+    id: '00ultwqjtqt4VCcS24x6',
+    user_type: 'Groomer',
+    avatarUrl: faker.image.avatar(),
+    email: 'llama004@maildrop.cc',
+    name: `${faker.name.firstName()} ${faker.name.lastName()}`,
+    certifications: ['Dogs', 'Cats'],
+  },
+  {
+    id: '00ultwz1n9ORpNFc04x6',
+    user_type: 'Customer',
+    avatarUrl: faker.image.avatar(),
+    email: 'llama005@maildrop.cc',
+    name: `${faker.name.firstName()} ${faker.name.lastName()}`,
+    certifications: null,
+  },
+  {
+    id: '00u13omswyZM1xVya4x7',
+    user_type: 'Groomer',
+    avatarUrl: faker.image.avatar(),
+    email: 'llama006@maildrop.cc',
+    name: `${faker.name.firstName()} ${faker.name.lastName()}`,
+    certifications: ['Dogs', 'Cats'],
+  },
+  {
+    id: '00u13ol5x1kmKxVJU4x7',
+    user_type: 'Customer',
+    avatarUrl: faker.image.avatar(),
+    email: 'llama007@maildrop.cc',
+    name: `${faker.name.firstName()} ${faker.name.lastName()}`,
+    certifications: null,
+  },
+  {
+    id: '00u13oned0U8XP8Mb4x7',
+    user_type: 'Groomer',
+    avatarUrl: faker.image.avatar(),
+    email: 'llama008@maildrop.cc',
+    name: `${faker.name.firstName()} ${faker.name.lastName()}`,
+    certifications: ['Dogs', 'Cats'],
+  },
+];
 
 exports.seed = async (knex) => {
   // Insert profiles
