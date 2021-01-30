ce1a808636d90e8268b15d869cca9cdcf1cd867a
Cody Brewer <codybrewerdev@gmail.com>
2021-01-30 12:49:23 -0700

    refactor(customerPetRouter.js): remove unused import AppointmentsModel

--- api/customer_pet/customerPetRouter.js
+++ api/customer_pet/customerPetRouter.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const authRequired = require('../middleware/authRequired');
 const CustomerPetModel = require('./customerPetModel');
-const AppointmentModel = require('../appointments/appointmentsModel');
 const router = express.Router();
 const {
   validateCustomerPetID,
