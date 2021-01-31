a81249cf16feea26e45192c2b7678a963b7eebbd
Cody Brewer <codybrewerdev@gmail.com>
2021-01-31 15:20:59 -0700

    feat(008_appointments.js): seed appointments for each seeded customer pet

--- data/seeds/008_appointments.js
+++ data/seeds/008_appointments.js
@@ -1,3 +1,11 @@
+const faker = require('faker');
+
+// all customer pets should have had a least one previous appointment
+// half customer pets have one pending
+// half customer pets have one Approved
+
+const unixTime = Math.floor(Date.now() / 1000);
+
 exports.seed = (knex) => {
   // Deletes ALL existing entries
   return knex('appointments')
@@ -12,23 +20,141 @@ exports.seed = (knex) => {
           location_service_id: 1,
           service_provider_name: 'Pro Grooming Services',
           status: 'Pending',
-          appointment_date_time: 1611878560,
+          appointment_date_time: Math.floor(
+            new Date(faker.date.soon()).getTime() / 1000
+          ),
           duration: 60,
         },
         {
           customer_id: '00ulthapbErVUwVJy4x6',
           groomer_id: '00ultwew80Onb2vOT4x6',
-<<<<<<< HEAD:data/seeds/008_appointments.js
           pet_id: 1,
           location_service_id: 1,
           service_provider_name: 'Pro Grooming Services',
-=======
-          pet_id: 0,
-          location_service_id: 0,
-          service_provider_name: 'Buddy',
->>>>>>> ef06d9f388981ec4a1b8b6dcbe04df62b05bfee8:data/seeds/011_appointments.js
           status: 'Completed',
-          appointment_date_time: 1611878560,
+          appointment_date_time: Math.floor(
+            new Date(faker.date.recent()).getTime() / 1000
+          ),
+          duration: 60,
+        },
+        {
+          customer_id: '00ulthapbErVUwVJy4x6',
+          groomer_id: '00ultwew80Onb2vOT4x6',
+          pet_id: 2,
+          location_service_id: 1,
+          service_provider_name: 'Pro Grooming Services',
+          status: 'Approved',
+          appointment_date_time: Math.floor(
+            new Date(faker.date.soon()).getTime() / 1000
+          ),
+          duration: 60,
+        },
+        {
+          customer_id: '00ulthapbErVUwVJy4x6',
+          groomer_id: '00ultwew80Onb2vOT4x6',
+          pet_id: 2,
+          location_service_id: 1,
+          service_provider_name: 'Pro Grooming Services',
+          status: 'Completed',
+          appointment_date_time: Math.floor(
+            new Date(faker.date.recent()).getTime() / 1000
+          ),
+          duration: 60,
+        },
+        {
+          customer_id: '00ulthapbErVUwVJy4x6',
+          groomer_id: '00ultwew80Onb2vOT4x6',
+          pet_id: 3,
+          location_service_id: 1,
+          service_provider_name: 'Pro Grooming Services',
+          status: 'Completed',
+          appointment_date_time: Math.floor(
+            new Date(faker.date.recent()).getTime() / 1000
+          ),
+          duration: 60,
+        },
+        {
+          customer_id: '00ulthapbErVUwVJy4x6',
+          groomer_id: '00ultwew80Onb2vOT4x6',
+          pet_id: 4,
+          location_service_id: 1,
+          service_provider_name: 'Pro Grooming Services',
+          status: 'Completed',
+          appointment_date_time: Math.floor(
+            new Date(faker.date.recent()).getTime() / 1000
+          ),
+          duration: 60,
+        },
+        {
+          customer_id: '00ultx74kMUmEW8054x6',
+          groomer_id: '00ultwew80Onb2vOT4x6',
+          pet_id: 5,
+          location_service_id: 1,
+          service_provider_name: 'Pro Grooming Services',
+          status: 'Completed',
+          appointment_date_time: Math.floor(
+            new Date(faker.date.recent()).getTime() / 1000
+          ),
+          duration: 60,
+        },
+        {
+          customer_id: '00ultwz1n9ORpNFc04x6',
+          groomer_id: '00ultwew80Onb2vOT4x6',
+          pet_id: 6,
+          location_service_id: 1,
+          service_provider_name: 'Pro Grooming Services',
+          status: 'Completed',
+          appointment_date_time: Math.floor(
+            new Date(faker.date.recent()).getTime() / 1000
+          ),
+          duration: 60,
+        },
+        {
+          customer_id: '00u13ol5x1kmKxVJU4x7',
+          groomer_id: '00ultwew80Onb2vOT4x6',
+          pet_id: 7,
+          location_service_id: 1,
+          service_provider_name: 'Pro Grooming Services',
+          status: 'Completed',
+          appointment_date_time: Math.floor(
+            new Date(faker.date.recent()).getTime() / 1000
+          ),
+          duration: 60,
+        },
+        {
+          customer_id: '00ultx74kMUmEW8054x6',
+          groomer_id: '00ultwew80Onb2vOT4x6',
+          pet_id: 5,
+          location_service_id: 1,
+          service_provider_name: 'Pro Grooming Services',
+          status: 'Approved',
+          appointment_date_time: Math.floor(
+            new Date(faker.date.soon()).getTime() / 1000
+          ),
+          duration: 60,
+        },
+        {
+          customer_id: '00ultwz1n9ORpNFc04x6',
+          groomer_id: '00ultwew80Onb2vOT4x6',
+          pet_id: 6,
+          location_service_id: 1,
+          service_provider_name: 'Pro Grooming Services',
+          status: 'Pending',
+          appointment_date_time: Math.floor(
+            new Date(faker.date.soon()).getTime() / 1000
+          ),
+          duration: 60,
+        },
+        {
+          customer_id: '00u13ol5x1kmKxVJU4x7',
+          groomer_id: '00ultwew80Onb2vOT4x6',
+          pet_id: 7,
+          location_service_id: 1,
+          service_provider_name: 'Pro Grooming Services',
+          status: 'Approved',
+          appointment_date_time: Math.floor(
+            new Date(faker.date.soon()).getTime() / 1000
+          ),
           duration: 60,
         },
       ]);
