005d9bae0da74015ed54df5388e516d3842f21d9
Cody Brewer <codybrewerdev@gmail.com>
2021-01-31 14:48:54 -0700

    feat(007_customer_pets.js): seed customer_pets for each seeded customer
    llama001 has 4 pets &  llama003, llama005, llama007 each have one pet

--- data/seeds/007_customer_pets.js
+++ data/seeds/007_customer_pets.js
@@ -1,3 +1,5 @@
+const faker = require('faker');
+
 exports.seed = (knex) => {
   // Deletes ALL existing entries
   return knex('customer_pets')
@@ -7,12 +9,66 @@ exports.seed = (knex) => {
       return knex('customer_pets').insert([
         {
           customer_id: '00ulthapbErVUwVJy4x6',
+          animal_id: 1,
+          pet_name: faker.name.firstName() + ' ' + faker.name.lastName(),
+          color: faker.commerce.color(),
+          date_of_birth: faker.date.between('1-1-2015', Date(Date.now())),
+          image_url: 'https://placeimg.com/640/480/animals/cats',
+          health_issue: 'Overt Cuteness',
+        },
+        {
+          customer_id: '00ulthapbErVUwVJy4x6',
+          animal_id: 1,
+          pet_name: faker.name.firstName() + ' ' + faker.name.lastName(),
+          color: faker.commerce.color(),
+          date_of_birth: faker.date.between('1-1-2015', Date(Date.now())),
+          image_url: 'https://placeimg.com/640/480/animals/cats',
+          health_issue: 'Overt Cuteness',
+        },
+        {
+          customer_id: '00ulthapbErVUwVJy4x6',
           animal_id: 2,
-          pet_name: 'Rabby',
-          color: 'Red',
-          date_of_birth: '2020-11-02T00:00:00.000Z',
-          image_url: 'https://s3.amazonaws.com/1.jpg',
-          health_issue: 'Diabetes',
+          pet_name: faker.name.firstName() + ' ' + faker.name.lastName(),
+          color: faker.commerce.color(),
+          date_of_birth: faker.date.between('1-1-2015', Date(Date.now())),
+          image_url: 'https://placeimg.com/640/480/animals/dogs',
+          health_issue: 'Puppy Breath',
+        },
+        {
+          customer_id: '00ulthapbErVUwVJy4x6',
+          animal_id: 2,
+          pet_name: faker.name.firstName() + ' ' + faker.name.lastName(),
+          color: faker.commerce.color(),
+          date_of_birth: faker.date.between('1-1-2015', Date(Date.now())),
+          image_url: 'https://placeimg.com/640/480/animals/dogs',
+          health_issue: 'Puppy Breath',
+        },
+        {
+          customer_id: '00ultx74kMUmEW8054x6',
+          animal_id: 2,
+          pet_name: faker.name.firstName() + ' ' + faker.name.lastName(),
+          color: faker.commerce.color(),
+          date_of_birth: faker.date.between('1-1-2015', Date(Date.now())),
+          image_url: 'https://placeimg.com/640/480/animals/dogs',
+          health_issue: 'Puppy Breath',
+        },
+        {
+          customer_id: '00ultwz1n9ORpNFc04x6',
+          animal_id: 1,
+          pet_name: faker.name.firstName() + ' ' + faker.name.lastName(),
+          color: faker.commerce.color(),
+          date_of_birth: faker.date.between('1-1-2015', Date(Date.now())),
+          image_url: 'https://placeimg.com/640/480/animals/cats',
+          health_issue: 'Overt Cuteness',
+        },
+        {
+          customer_id: '00u13ol5x1kmKxVJU4x7',
+          animal_id: 1,
+          pet_name: faker.name.firstName() + ' ' + faker.name.lastName(),
+          color: faker.commerce.color(),
+          date_of_birth: faker.date.between('1-1-2015', Date(Date.now())),
+          image_url: 'https://placeimg.com/640/480/animals/cats',
+          health_issue: 'Overt Cuteness',
         },
       ]);
     });
